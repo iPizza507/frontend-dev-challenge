c9c3a00dd1727e3efa5a420291e5ae927f59a27e
iPizza <ianfigueroa507@gmail.com>
2022-07-07 12:30:07 -0300

    first commit

---
+++ moms/src/App.js
@@ -0,0 +1,12 @@
+//Components
+import HeaderNavBar from "./Components/Header/HeaderNavBar";
+function App() {
+  return (
+    <>
+      <HeaderNavBar></HeaderNavBar>
+      <h1>Estamos ok</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ moms/src/Components/Header/HeaderNavBar.js
@@ -0,0 +1,11 @@
+export const HeaderNavBar = () => {
+  return (
+    <>
+      <nav>
+        <h1>Header</h1>
+      </nav>
+    </>
+  );
+};
+
+export default HeaderNavBar;
